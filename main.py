42adbdd5a1d31c33e279563410373e8cca997606
christy-thomas-rit <148941878+christy-thomas-rit@users.noreply.github.com>
2025-01-29 21:30:48 +0530

    Remove redundant Flask app initialization and imports in main.py

--- main.py
+++ main.py
@@ -15,20 +15,3 @@ app.register_blueprint(advisor)
 
 app.run(debug=True,host='0.0.0.0',port=5009)
 
-from flask import *
-from public import *
-from admin import *
-from advisor import *
-
-
-app=Flask(__name__)
-
-app.secret_key='secret_key'
-
-app.register_blueprint(public)
-app.register_blueprint(admin)
-app.register_blueprint(advisor)
-
-
-app.run(debug=True,host='0.0.0.0',port=5009)
-
